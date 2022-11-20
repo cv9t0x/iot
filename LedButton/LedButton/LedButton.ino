ca948ef79e51646575be6526f4a7408ad07a63fc
Svyatoslav <sviatoslavnazin@gmail.com>
2022-11-20 19:08:46 +0800

    add LedButton task

---
+++ LedButton/LedButton/LedButton.ino
@@ -0,0 +1,59 @@
+#define led_pin 8
+#define btn_pin A3
+
+int curr, prev;
+bool turn_on = false;
+bool next = false;
+int brightness = 255;
+int delay = 300;
+
+void setup(){
+  pinMode(led_pin, OUTPUT);
+  pinMode(btn_pin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  curr = digitalRead(btn_pin);
+
+  if (curr != prev){
+    delay(10);
+
+    curr = digitalRead(btn_pin);
+    long time = millis();
+
+    while (digitalRead(btn_pin)){
+      if (millis() - time >= delay && turn_on){
+        while (digitalRead(btn_pin)){
+          if (next){
+            brightness = min(brightness + 1, 255);
+          }
+          else{
+            brightness = max(brightness - 1, 20);
+          }
+
+          analogWrite(led_pin, brightness);
+          delay(10);
+        }
+
+        next = !next;
+      }
+
+      delay(10);
+    }
+    
+    if (millis() - time < delay){
+      if (curr){
+          turn_on = !turn_on;
+      }
+      if (turn_on){
+        analogWrite(led_pin, brightness);
+      }      
+      else{
+        analogWrite(led_pin, 0);
+      }
+    }
+  }
+  
+  prev = curr;
+}
